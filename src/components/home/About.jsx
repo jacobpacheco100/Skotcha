d856c069c15d081ec4bbdff35db88eca3e0f9627
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-09-05 12:41:04 -0400

    add: about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const About = () => {
-  return <div className='container mt-52'>asd</div>
+  return (
+    <div className='container mt-52 flex flex-col items-center justify-center space-y-10 lg:flex-row lg:space-x-10 lg:space-y-0'>
+      <div className='h-[379px] w-full max-w-[492px] bg-gray-2 rounded-xl'></div>
+      <article>
+        <h3 className='h3'>About</h3>
+        <h2 className='h2'>What is Skotcha?</h2>
+        <p className='text max-w-[492px]'>
+          Skotcha is an advanced online task managing application. No sign in
+          required, instead all your data is saved to you local storage. With
+          Skotcha you can effortlessy and efficiently manage a complex array of
+          tasks and organize your work.
+        </p>
+        <button className='button-1 bg-gray-1'>Learn More</button>
+      </article>
+    </div>
+  )
 }
 
 export default About
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -27,7 +27,17 @@ const Navbar = () => {
         className={`w-[70%] h-screen fixed bg-gray-1 top-0 right-0 z-20 translate-x-[100%] ease-in-out duration-300 ${
           isMenu && 'translate-x-[0%]'
         }`}
-      ></div>
+      >
+        <div className='flex flex-col items-center justify-between space-y-9 px-10 mt-28 text-xl font-bold'>
+          <a href='#'>How it Works</a>
+          <a href='#'>About</a>
+          <a href='#'>Contact</a>
+          <span></span>
+          <button className='button-1 bg-primary font-medium text-lg'>
+            My Tasks
+          </button>
+        </div>
+      </div>
 
       {isMenu && (
         <div className='w-screen h-screen bg-dark opacity-70 z-10 fixed'></div>
